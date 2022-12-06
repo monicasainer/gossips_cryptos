c4e502f61beeb8686a316d48b1355fc253e9df36
Mark Sarifidis <sarifidismark@gmail.com>
2022-12-06 12:22:03 +0100

    Update data file with currency converter

--- gossips_cryptos/model/data.py
+++ gossips_cryptos/model/data.py
@@ -4,6 +4,16 @@ import seaborn as sns
 import requests
 import os
 
+def currency_converter()-> pd.DataFrame:
+    """Finding a fitting currency conversion API (Based on USD) and requesting the data as a function """
+
+    APIkey = "j4YVbY4DsPe2bRdqchwWzvccCljEhg2EhlPyfrEN"
+    url = f"https://api.freecurrencyapi.com/v1/latest?apikey={APIkey}"
+    response = requests.get(url).json()
+    df = pd.DataFrame(response['data'], index=[0])
+    return df
+
+
 def fgindex() -> pd.DataFrame:
     """returns a dataframe of fear and greed index with columns:
     [value],[value_classification],[timestamp],[time_until_update] """
