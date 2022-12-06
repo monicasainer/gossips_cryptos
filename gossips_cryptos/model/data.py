b335bd690fbfe8f69631002d1bfeb0dcdbc71db0
Monica Sainer Boto <monicasainerboto@gmail.com>
2022-12-06 11:35:31 +0100

    Added fgindex and btc functions and variables to env

--- gossips_cryptos/model/data.py
+++ gossips_cryptos/model/data.py
@@ -9,7 +9,7 @@ import os
 ## Getting data:
 
 def fgindex() -> pd.DataFrame:
-    """returns a dataframe of fear and Greed index with columns:
+    """returns a dataframe of fear and greed index with columns:
     [value],[value_classification],[timestamp],[time_until_update] """
 
     url = 'https://api.alternative.me/fng/'
@@ -17,14 +17,10 @@ def fgindex() -> pd.DataFrame:
     response = requests.get(url,params).json()
     fg = pd.DataFrame(response['data'])
     return fg
-# # Converting string to timestamp.
-# Cleaning part: fg['timestamp'] = pd.to_datetime(fg['timestamp'],dayfirst=True)
-
-
 
 
 # Closing prices
-def prices(crypto):
+def prices(crypto)-> pd.DataFrame:
     """returns a dataframe with columns:
     ['time_period_start'], ['time_period_end'], ['time_open'], ['time_close'],
     ['rate_open'], ['rate_high'], ['rate_low'], ['rate_close'] """
@@ -36,28 +32,3 @@ def prices(crypto):
     response = requests.get(url, headers=headers).json()
     crypto = pd.DataFrame(response)
     return crypto
-
-
-## Cleaning part
-##BTC_EUR.rename(columns={'time_close':'timestamp'},inplace=True)
-
-##BTC_EUR['timestamp'] = BTC_EUR['timestamp'].apply(lambda x: x[0:10]) #Removing the time
-
-##BTC_EUR['timestamp']=pd.to_datetime(BTC_EUR['timestamp'],yearfirst=True)
-##BTC_EUR=BTC_EUR[['timestamp','rate_close']] #Getting two columns
-
-
-# Index and Crypto together
-#df=fg.merge(BTC_EUR,how='left',on='timestamp')
-#df
-
-
-
-# Exchange rates base asset identifier
-#url = 'https://rest.coinapi.io/v1/assets'
-#headers = {'X-CoinAPI-Key' : 'CC32A4FC-471F-4200-8788-6C980B2C8CAF'} #API-key
-#response = requests.get(url, headers=headers).json()
-
-
-#market=[i['asset_id'] for i in response]
-#sns.scatterplot(df['value_classification'],df['rate_close'])
